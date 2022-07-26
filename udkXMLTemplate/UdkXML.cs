5ce6df1061cb899eb7a7b6ea62f262160c845728
Uygar Demir KOÇ <kocuygar@yahoo.com.tr>
2022-07-26 18:36:07 +0300

    İlk Sürüm

---
+++ udkXMLTemplate/UdkXML.cs
@@ -0,0 +1,82 @@
+﻿namespace udkXMLTemplate
+{
+    using System.IO.Compression;
+    public class UdkXML
+    {
+        private static Random random = new Random();
+        private static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private static void DeleteDirectory(string target_dir)
+        {
+            string[] files = Directory.GetFiles(target_dir);
+            string[] dirs = Directory.GetDirectories(target_dir);
+
+            foreach (string file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            foreach (string dir in dirs)
+            {
+                DeleteDirectory(dir);
+            }
+
+            Directory.Delete(target_dir, false);
+        }
+
+
+        public bool applyTemplate(String file, String output, KeyValuePair<String, String>[] template)
+        {
+            try
+            {
+                //geçici klasör oluştur.
+                String tempFolder = AppDomain.CurrentDomain.BaseDirectory + RandomString(32) + @"\";
+
+                //arşivi çıkart.
+                ZipFile.ExtractToDirectory(file, tempFolder);
+
+                //tüm xml dosyalarını oku.
+                string[] xmlFiles = System.IO.Directory.GetFiles(tempFolder, "*.xml", SearchOption.AllDirectories);
+
+                //tüm xml dosyalarını tek tek aç.
+                String temp = "";
+                foreach (String xml in xmlFiles)
+                {
+                    //gerekli değişiklikleri yap
+                    temp = File.ReadAllText(xml);
+
+                    foreach (KeyValuePair<String, String> kvp in template)
+                    {
+                        temp = temp.Replace("{"+kvp.Key+"}", kvp.Value);
+                    }
+
+                    File.WriteAllText(xml, temp);
+                }
+
+                //klasörü tekrar paketle
+                //mevcutsa sil
+                if (File.Exists(output))
+                    File.Delete(output);
+                //paketi oluştur.
+                ZipFile.CreateFromDirectory(tempFolder, output);
+
+                //klasörü imha et.
+                DeleteDirectory(tempFolder);
+
+                return true;
+            }catch(Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+            
+
+        }
+    }
+}
---
+++ udkXMLTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using udkXMLTemplate;
+
+//udkXML nesnesi oluşturuyoruz
+UdkXML udkXML = new UdkXML();
+
+//şabloda değişmesi gereken noktaları KeyValuePair dizesi içerisine tanımlıyoruz.
+//*****
+//KeyValuePair'in key kısmına şablon dosyada {} arasına yazdığımız yer tutucu adı
+//gelmeli. Value kısmına ise yer tutucunun yerine gelecek veriyi ekliyoruz.
+KeyValuePair<String, String>[] template = { 
+    new KeyValuePair<String, String>("test1", "Dünya!"), 
+    new KeyValuePair<string, string>("test2", "iki"),
+    new KeyValuePair<string, string>("degisecek", "değişti!")
+};
+String path = @"C:\Users\Uygar_DK\source\repos\udkXMLTemplate\templateExamples\";
+String templateFile = path+@"example2.docx";
+String saveTo = path + @"test2.docx";
+//fonksiyonu çağırıyoruz. 
+//1.parametreye şablon dosya yolunu (String),
+//2.parametreye oluşan yeni dosyanın kaydedileceği konumu (String),
+//3.parametreye ise yukarıda oluşturduğumuz KeyValuePair dizesini (KeyValuePair[])
+//ekliyoruz. (Şu anda Key-Value değerleri yalnızca String olabilir)
+udkXML.applyTemplate(templateFile, saveTo, template);
+//Başarıyla kayıt gerçekleştiğinde program true değeri döndürecektir.
+//Herhangi bir hata durumunda false değeri dönecek ve hatayla ilgili ayrıntılar
+//konsol ekranına basılacaktır.
